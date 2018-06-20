35bf10e5d1a2e48f97509963dfe634d4f03f0675
golony6449 <bb0162@live.co.kr>
2018-06-20 23:29:54 +0900

    직선 추출 (with hough Tranform)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+import time
+import numpy as np
 
 from module import controller
 from module import capture
@@ -7,7 +9,42 @@ ctl = controller.Controller()
 capture = capture.Capture()
 
 # ctl.setDirection(0x4000)
-
+currentTIme = time.time()
 while True:
-    cv2.imshow("screenshot", capture.export())
+    img = capture.export()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 100, 200)
+
+    angle = np.pi / 180
+    lines = cv2.HoughLines(edges, 5, angle * 3, 1000)
+
+    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+
+    overSpeedMarker = img[730, 1500, 2]
+    print(overSpeedMarker)
+    if overSpeedMarker > 80:
+        cv2.putText(edges, "Warning: OverSpeed!!", (10, 40), 1, 1.2, (0, 0, 255))
+
+    if lines is None:
+        pass
+    else:
+        for i in range(len(lines)):
+            for rho, theta in lines[i]:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 2000 * (-b))
+                y1 = int(y0 + 2000 * (a))
+                x2 = int(x0 - 2000 * (-b))
+                y2 = int(y0 - 2000 * (a))
+
+                # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.line(edges, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.putText(edges, "Frame took {} seconds".format(time.time() - currentTIme), (10, 20), 1, 1.2, (255, 255, 255))
+    currentTIme = time.time()
+
+    cv2.imshow("edges", edges)
+    # cv2.imshow("lines", img)
     cv2.waitKey(1)
