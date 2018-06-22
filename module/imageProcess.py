ace6879278e757a5c7af3e226e6b47afb30783b8
golony6449 <bb0162@live.co.kr>
2018-06-22 19:31:07 +0900

    노이즈 제거를 위해 ROI 조정 및 세밀화, 모폴로지 연산 추가

--- module/imageProcess.py
+++ module/imageProcess.py
@@ -3,9 +3,10 @@ import numpy as np
 
 def roi(img):
     mask = np.zeros(img.shape, np.uint8)
-    mask[700:1080, 0:1920] = 255
-    mask[550:800, 530:1390] = 255
-    mask[440:650, 750: 1170] = 255
+    mask[800:1080, 300:1620] = 255
+    mask[600:800, 450: 1370] = 255
+    mask[450:600, 730:1190] = 255
+    mask[340:450, 850: 1070] = 255
     # cv2.imshow("img", img)
     # cv2.imshow("mask", mask)
     # cv2.waitKey()
@@ -13,3 +14,13 @@ def roi(img):
     res = cv2.bitwise_and(img, img, mask=mask)
     # cv2.imshow("result", res)
     return res
+
+def morp(img):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+    res1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    res2 = cv2.morphologyEx(res1, cv2.MORPH_OPEN, kernel2)
+
+
+    return res1
